51025c7c98576218e16de58c025943c65d4ea3ad
Edson <edsonmanuelcarballovera@gmail.com>
2022-09-17 16:31:43 -0500

    Api specification has been updated and automatic validation and publication has been setted

--- src/Routes/MongoController.py
+++ src/Routes/MongoController.py
@@ -1,15 +1,16 @@
 import json
 
+from bson import json_util
 from flask import Blueprint, request, Response
 
 from src.Config.MongoHandler import MongoHandler
-from src.Routes.HTTPStatus import RESOURCE_CREATED, NOT_ACCEPTABLE
+from src.Routes.HTTPStatus import RESOURCE_CREATED, NOT_ACCEPTABLE, OK, NO_CONTENT
 
 mongo_routes = Blueprint("mongo_routes", __name__)
 
 
-@mongo_routes.post("/database/<data_name>")
-def upload_data(data_name: str):
+@mongo_routes.post("/storage/<collection_name>")
+def upload_data(collection_name: str):
 	response = Response(status=NOT_ACCEPTABLE)
 	file_data = request.files["data"].stream
 	dict_keys = file_data.readline().decode("utf-8").strip().split(",")
@@ -21,9 +22,9 @@ def upload_data(data_name: str):
 			aux_object[dict_keys[index]] = row_values[index]
 		objects.append(aux_object)
 	if len(objects) != 0:
-		mongo = MongoHandler()
-		mongo.set_database("testingDB")
-		mongo.set_collection(data_name)
+		mongo: MongoHandler = MongoHandler()
+		mongo.set_database("storage")
+		mongo.set_collection(collection_name)
 		mongo.insert_many(objects)
 		response = Response(
 			json.dumps({"records_created": len(objects)}),
@@ -32,6 +33,16 @@ def upload_data(data_name: str):
 	return response
 
 
-@mongo_routes.get("/database/<name>")
-def get_database(id: str):
-	return "database endpoint still not working"
+@mongo_routes.get("/storage/<collection_name>")
+def get_database(collection_name: str):
+	mongo: MongoHandler = MongoHandler()
+	mongo.set_database("storage")
+	mongo.set_collection(collection_name)
+	results = mongo.find_all()
+	response = Response(status=NO_CONTENT)
+	if len(results) > 0:
+		response = Response(
+			json_util.dumps(results),
+			status=OK,
+			mimetype="application/json")
+	return response
