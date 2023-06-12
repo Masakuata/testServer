0a91e66d7b246d07b8c946bc40420b8b2cd2bbb2
Edson <edsonmanuelcarballovera@gmail.com>
2023-06-12 02:11:55 -0600

    Se ha agrado flexibilidad para guardar y obtener information con un ID unico

--- src/Routes/MongoController.py
+++ src/Routes/MongoController.py
@@ -8,7 +8,8 @@ from flask import Blueprint, request, Response
 from xfss.RemoteSession import RemoteSession
 
 from src.Config.MongoHandler import MongoHandler
-from src.Routes.HTTPStatus import RESOURCE_CREATED, OK, NO_CONTENT, BAD_REQUEST, NOT_ACCEPTABLE, INTERNAL_SERVER_ERROR
+from src.Routes.HTTPStatus import RESOURCE_CREATED, OK, NO_CONTENT, BAD_REQUEST, NOT_ACCEPTABLE, INTERNAL_SERVER_ERROR, \
+	NOT_FOUND
 
 mongo_routes = Blueprint("mongo_routes", __name__)
 
@@ -73,13 +74,64 @@ def upload_json(collection_name: str):
 		mongo.set_collection(collection_name)
 
 		response = Response(status=INTERNAL_SERVER_ERROR)
-		if mongo.insert_many(payload):
+		if isinstance(payload, list) and mongo.insert_many(payload):
 			response = Response(
 				json.dumps({"records_created": len(payload)}),
 				status=RESOURCE_CREATED)
+		if isinstance(payload, dict) and mongo.insert_one(payload):
+			response = Response(status=RESOURCE_CREATED)
 
 	del payload
 	del mongo
+	return response
+
+
+@mongo_routes.post("/storage/json/<collection_name>/<identifier>")
+@RemoteSession.requires_token
+def create_json(collection_name: str, identifier: str):
+	payload = request.json
+	response = Response(status=NOT_ACCEPTABLE)
+	if payload is not None and not isinstance(payload, list):
+		mongo: MongoHandler = MongoHandler()
+		mongo.set_database("randomStorage")
+		mongo.set_collection(collection_name)
+
+		response = Response(status=BAD_REQUEST)
+		values: dict = {
+			"id": identifier,
+			"information": payload
+		}
+
+		stored_info = mongo.find_one({"id": identifier})
+		if stored_info is not None and stored_info.get("information") is not None:
+			stored_info: dict = stored_info["information"]
+			for key in values["information"].keys():
+				stored_info[key] = values["information"][key]
+			values["information"] = stored_info
+
+			if mongo.collection.replace_one({"id": identifier}, values).modified_count > 0:
+				response = Response(status=RESOURCE_CREATED)
+		elif mongo.insert_one(values):
+			response = Response(status=RESOURCE_CREATED)
+	return response
+
+
+@mongo_routes.get("/storage/json/<collection_name>/<identifier>")
+@RemoteSession.requires_token
+def get_json(collection_name: str, identifier: str):
+	mongo: MongoHandler = MongoHandler()
+	mongo.set_database("randomStorage")
+	mongo.set_collection(collection_name)
+
+	response = Response(status=NOT_FOUND)
+
+	stored_info: dict = mongo.find_one({"id": identifier})
+	if stored_info is not None and stored_info["information"] is not None:
+		response = Response(
+			json.dumps(stored_info["information"]),
+			status=OK,
+			mimetype="application/json"
+		)
 	return response
 
 
